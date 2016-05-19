f878194d250a709ca0183b5df43842cc16ac7451
bryantbello <bryant.bello@outlook.com>
2016-05-18 20:40:29 -0400

    test

--- timer.js
+++ timer.js
@@ -1,14 +1,14 @@
 var TimerExample = React.createClass({
 
-    getInitialState: function(){
+    getInitialState: function() {
 
         // This is called before our render function. The object that is 
-        // returned is assigned to this.state, so we can use it later.
+        // returned is assigned to this.state, so we can use it later...
 
         return { elapsed: 0 };
     },
 
-    componentDidMount: function(){
+    componentDidMount: function() {
 
         // componentDidMount is called by react when the component 
         // has been rendered on the page. We can set the interval here:
@@ -16,7 +16,7 @@ var TimerExample = React.createClass({
         this.timer = setInterval(this.tick, 50);
     },
 
-    componentWillUnmount: function(){
+    componentWillUnmount: function() {
 
         // This method is called immediately before the component is removed
         // from the page and destroyed. We can clear the interval here:
@@ -24,30 +24,31 @@ var TimerExample = React.createClass({
         clearInterval(this.timer);
     },
 
-    tick: function(){
+    tick: function() {
 
         // This function is called every 50 ms. It updates the 
         // elapsed counter. Calling setState causes the component to be re-rendered
 
-        this.setState({elapsed: new Date() - this.props.start});
+        this.setState({ elapsed: new Date() - this.props.start });
     },
 
     render: function() {
-        
+
         var elapsed = Math.round(this.state.elapsed / 100);
 
         // This will give a number with one digit after the decimal dot (xx.x):
-        var seconds = (elapsed / 10).toFixed(1);    
+        var seconds = (elapsed / 10).toFixed(1);
 
         // Although we return an entire <p> element, react will smartly update
         // only the changed parts, which contain the seconds variable.
 
-        return <p>This example was started <b>{seconds} seconds</b> ago.</p>;
+        return <p > This example was started < b > { seconds }
+        seconds < /b> ago.</p > ;
     }
 });
 
 
-ReactDOM.render(
-    <TimerExample start={Date.now()} />,
+ReactDOM.render( < TimerExample start = { Date.now() }
+    />,
     document.getElementById('container')
 );
